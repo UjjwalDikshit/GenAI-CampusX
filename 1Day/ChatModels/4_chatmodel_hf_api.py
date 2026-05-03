fff19cb2b3a848ada8ada9c047bb79b8628768a4
Ujjwal Dikshit <ujjwaldikshit1@gmail.com>
2026-05-03 13:57:45 +0530

    fixed token

--- 1Day/ChatModels/4_chatmodel_hf_api.py
+++ 1Day/ChatModels/4_chatmodel_hf_api.py
@@ -1,13 +1,13 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
-
+import os
 load_dotenv()
 
 llm = HuggingFaceEndpoint( # which model you wnat to use
     repo_id="mistralai/Mistral-7B-Instruct-v0.2",
     task="text-generation",
-    huggingfacehub_api_token="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"
-
+    # huggingfacehub_api_token="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"
+    huggingfacehub_api_token = os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
 )
 
 model = ChatHuggingFace(llm=llm)
